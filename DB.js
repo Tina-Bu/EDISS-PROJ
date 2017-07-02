e640dbffb4cec10d0b594fc1d94a1f94030b0caf
TinaHongBu <butianhong@outlook.com>
2017-07-02 17:25:43 -0400

    changed db name

--- DB.js
+++ DB.js
@@ -53,7 +53,7 @@ exports.insert = function(query, callback) {
 Code to create the database table:
 For users:
 
-CREATE TABLE p2_user (
+CREATE TABLE user (
   fname VARCHAR(255) NOT NULL,
   lname VARCHAR(255) NOT NULL,
   address VARCHAR(255),
@@ -65,15 +65,15 @@ CREATE TABLE p2_user (
   password VARCHAR(255) NOT NULL,
   user_type_id INT DEFAULT 1
 );
-ALTER TABLE p2_user ADD CONSTRAINT UQ_Name UNIQUE (username);
+ALTER TABLE user ADD CONSTRAINT UQ_Name UNIQUE (username);
 
-CREATE TABLE p2_usertype (
+CREATE TABLE usertype (
 id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 user_type ENUM('customer', 'admin')
 );
 
-INSERT INTO p2_usertype (user_type) VALUES ('customer');
-INSERT INTO p2_usertype (user_type) VALUES ('admin');
+INSERT INTO usertype (user_type) VALUES ('customer');
+INSERT INTO usertype (user_type) VALUES ('admin');
 +----+-----------+
 | id | user_type |
 +----+-----------+
@@ -81,35 +81,35 @@ INSERT INTO p2_usertype (user_type) VALUES ('admin');
 |  2 | admin     |
 +----+-----------+
 
-INSERT INTO p2_user (fname, lname, address, city, state, zip, email, username, password) VALUES ("Tina", "Bu", "732 S. Millvale Ave", "Pittsburgh", "PA", 15213, "butianhong@outlook.com", "tbu", "tbu");
-INSERT INTO p2_user (fname, lname, address, city, state, zip, email, username, password) VALUES ("Tina", "Tiiii", "Center Ave", "Pittsburgh", "PA", 15213, "tiii@outlook.com", "tita", "tita");
-INSERT INTO p2_user (fname, lname, username, password, user_type_id) VALUES ("Jenny", "Admin", "jadmin", "admin", 2);
+INSERT INTO user (fname, lname, address, city, state, zip, email, username, password) VALUES ("Tina", "Bu", "732 S. Millvale Ave", "Pittsburgh", "PA", 15213, "butianhong@outlook.com", "tbu", "tbu");
+INSERT INTO user (fname, lname, address, city, state, zip, email, username, password) VALUES ("Tina", "Tiiii", "Center Ave", "Pittsburgh", "PA", 15213, "tiii@outlook.com", "tita", "tita");
+INSERT INTO user (fname, lname, username, password, user_type_id) VALUES ("Jenny", "Admin", "jadmin", "admin", 2);
 */
 
 /*
  Code to create the database table:
  For products:
 
-CREATE TABLE p2_prdct(
+CREATE TABLE product(
     asin VARCHAR(255) PRIMARY KEY,
     productName VARCHAR(255) NOT NULL,
     productDescription VARCHAR(255),
     productGroup VARCHAR(255)
 );
 
- ALTER TABLE p2_prdct ADD CONSTRAINT UQ_Name UNIQUE (asin);
+ ALTER TABLE product ADD CONSTRAINT UQ_Name UNIQUE (asin);
 
- INSERT INTO p2_prdct (asin, productName, productDescription, productGroup) VALUES (1, "Laptop", "Macbook 2015", "Electronics");
- INSERT INTO p2_prdct (asin, productName, productDescription, productGroup) VALUES (2, "Zen and Motorcycle maintenance", "Best selling spiritual book", "Book");
- INSERT INTO p2_prdct (asin, productName, productDescription, productGroup) VALUES (3, "Grounded Coffee", "Starbucks coffee", "Food");
+ INSERT INTO product (asin, productName, productDescription, productGroup) VALUES (1, "Laptop", "Macbook 2015", "Electronics");
+ INSERT INTO product (asin, productName, productDescription, productGroup) VALUES (2, "Zen and Motorcycle maintenance", "Best selling spiritual book", "Book");
+ INSERT INTO product (asin, productName, productDescription, productGroup) VALUES (3, "Grounded Coffee", "Starbucks coffee", "Food");
 
  */
 
 /*
 For testing code to run, delete all from tables first.
 
-DELETE FROM p2_user;
-DELETE FROM p2_prdct;
-INSERT INTO p2_user (fname, lname, username, password, user_type_id) VALUES ("Jenny", "Admin", "jadmin", "admin", 2);
+DELETE FROM user;
+DELETE FROM product;
+INSERT INTO user (fname, lname, username, password, user_type_id) VALUES ("Jenny", "Admin", "jadmin", "admin", 2);
 
  */
