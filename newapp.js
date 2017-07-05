ee251ead4e61d7c58241856bdb2d1b19ed72fa38
TinaHongBu <butianhong@outlook.com>
2017-07-05 13:08:58 -0400

    change client session setting

--- newapp.js
+++ newapp.js
@@ -15,8 +15,8 @@ app.use(bodyParser.urlencoded({ extended: false}));
 app.use(clientSession({
   cookieName: 'session', // cookie name dictates the key name added to the request object
   secret: 'blargadeeblargblarg', // should be a large unguessable string
-  duration: 24 * 60 * 60 * 1000, // how long the session will stay valid in ms
-  activeDuration: 1000 * 60 * 5 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
+  duration: 15 * 60 * 1000, // how long the session will stay valid in ms
+  activeDuration: 1000 * 60 * 15 // if expiresIn < activeDuration, the session will be extended by activeDuration milliseconds
 }));
 
 app.set('port', process.env.PORT || 3000);
